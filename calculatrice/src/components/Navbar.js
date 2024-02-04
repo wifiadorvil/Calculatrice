3e00ee9c5b02fbd2cc34b5dce6da88168a4ea205
wifiadorvil <e2053716@cmaisonneuve.qc.ca>
2024-02-04 05:43:51 -0500

    final2

--- calculatrice/src/components/Navbar.js
+++ calculatrice/src/components/Navbar.js
@@ -1,11 +1,9 @@
 import React from "react";
-import logo from "../calccul.png";
 
 export default function NavBar()
 {
 return(
     <nav>
-        <img src={logo} className="nav--icon"></img>
         <h3 className="nav--logo_text">Calculator</h3>
     </nav>
 )
